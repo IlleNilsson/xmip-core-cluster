c527ae5d3f090e1ba69298c1d67a142e3fe063ee
Ilian Nilsson <ilian.nilsson@live.se>
2026-08-27 09:17:49 +0200

    Name the crate after its repository; take what this module owns

---
+++ target/debug/deps/xmip_core_cluster-629c0c5429e6dad1.d
@@ -0,0 +1,7 @@
+D:\Repos\Xmip\modules\foundation\cluster\target\debug\deps\xmip_core_cluster-629c0c5429e6dad1.d: src\lib.rs
+
+D:\Repos\Xmip\modules\foundation\cluster\target\debug\deps\libxmip_core_cluster-629c0c5429e6dad1.rlib: src\lib.rs
+
+D:\Repos\Xmip\modules\foundation\cluster\target\debug\deps\libxmip_core_cluster-629c0c5429e6dad1.rmeta: src\lib.rs
+
+src\lib.rs:
---
+++ target/debug/deps/xmip_core_cluster-8f4adb503a0a5c0a.d
@@ -0,0 +1,5 @@
+D:\Repos\Xmip\modules\foundation\cluster\target\debug\deps\xmip_core_cluster-8f4adb503a0a5c0a.d: src\lib.rs
+
+D:\Repos\Xmip\modules\foundation\cluster\target\debug\deps\xmip_core_cluster-8f4adb503a0a5c0a.exe: src\lib.rs
+
+src\lib.rs:
